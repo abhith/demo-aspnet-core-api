36ba574bd17b7bb223db75b6b1a225c783848090
Abhith <dearabhith@gmail.com>
2020-09-26 11:49:32 +0400

    chore: initial setup

---
+++ src/Services/Complaints/Azakaw.Complaints.API/Controllers/ComplaintsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Azakaw.Complaints.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ComplaintsController : ControllerBase
+    {
+        private readonly ILogger<ComplaintsController> _logger;
+
+        public ComplaintsController(ILogger<ComplaintsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
