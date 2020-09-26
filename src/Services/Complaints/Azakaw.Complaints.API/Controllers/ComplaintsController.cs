efdb991e9d41b4a487229fe3594a6be2a00fcebe
Abhith <dearabhith@gmail.com>
2020-09-26 14:44:34 +0400

    chore: add api documentation

--- src/Services/Complaints/Azakaw.Complaints.API/Controllers/ComplaintsController.cs
+++ src/Services/Complaints/Azakaw.Complaints.API/Controllers/ComplaintsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Azakaw.Complaints.API.Application.Commands;
+using Azakaw.Complaints.API.Application.Models;
 using Azakaw.Complaints.API.Application.Queries;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +11,7 @@ namespace Azakaw.Complaints.API.Controllers
 {
     [ApiVersion("1.0")]
     [Route("v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
     public class ComplaintsController : ApiControllerBase
     {
         private readonly IComplaintQueries _complaintQueries;
@@ -20,8 +23,15 @@ namespace Azakaw.Complaints.API.Controllers
             _complaintQueries = complaintQueries;
         }
 
+        /// <summary>
+        /// Creates new complaint
+        /// </summary>
+        /// <param name="createComplaintCommand"></param>
+        /// <param name="apiVersion"></param>
+        /// <returns>Id of the newly created complaint</returns>
         //[Authorize(Policy = Application.Authorization.Policies.ComplaintWrite)]
         [HttpPost("", Name = nameof(CreateComplaint))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<ActionResult> CreateComplaint([FromBody] CreateComplaintCommand createComplaintCommand, ApiVersion apiVersion)
         {
             var newComplaintId = await Mediator.Send(createComplaintCommand);
@@ -29,9 +39,14 @@ namespace Azakaw.Complaints.API.Controllers
             return CreatedAtAction(nameof(GetComplaint), new { complaintId = newComplaintId, version = apiVersion.ToString() }, new { id = newComplaintId });
         }
 
+        /// <summary>
+        /// Get complaint details by complaintId
+        /// </summary>
+        /// <param name="complaintId">complaint id</param>
+        /// <returns></returns>
         //[Authorize(Policy = Application.Authorization.Policies.ComplaintRead)]
         [HttpGet("{complaintId}", Name = nameof(GetComplaint))]
-        public async Task<ActionResult> GetComplaint(Guid complaintId)
+        public async Task<ActionResult<ComplaintResult>> GetComplaint(Guid complaintId)
         {
             var cashbackRequest = await _complaintQueries.GetComplaintByIdAsync(complaintId);
 
